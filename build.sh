52c24f0109ab8539c2c660195b8735e384d794f0
lwklhj <woonkit.liu.sg@gmail.com>
2024-02-16 01:47:03 +0800

    Corrected libtorch zip file name

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ echo "Configuring and building Thirdparty/libtorch ..."
 cd Thirdparty
 if [ ! -d libtorch ]; then
   wget https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.2.0%2Bcu118.zip \
-  && unzip libtorch-cxx11-abi-shared-with-deps-2.2.0+cu121.zip \
-  && rm libtorch-cxx11-abi-shared-with-deps-2.2.0+cu121.zip
+  && unzip libtorch-cxx11-abi-shared-with-deps-2.2.0+cu118.zip \
+  && rm libtorch-cxx11-abi-shared-with-deps-2.2.0+cu118.zip
 fi
 cd ../
 
